7ab1d53a643f4d776a2f70a1fb7e9b39ac53f8f3
Alberto Hernandez <740821@student.dusd.net>
2023-10-25 10:55:10 -0700

    made morre scrpts
    made more sccripts

---
+++ ScriptingProjectP3Hernandez/Assets/Scripts/Varaibles.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+public class Varaibles : MonoBehaviour
+{
+    int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
+ int MultiplyerByTwo(int number)
+{
+    int result;
+    result = 2;
+    return result;
+}
